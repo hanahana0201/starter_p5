b18b0a7b2ea36a10c859f754e6e5a287e904706c
Masatoshi Hanai <hanahana0201@gmail.com>
2021-09-03 11:04:05 +0900

    Add Color Schemes.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,19 @@
+const w = 5000
+
+// Color Scheme (light, dark)
+const colorSchemes = []
+// Blue
+colorSchemes[0] = ["#0fb4e1", "#07a0dc"]
+// Red
+colorSchemes[1] = ["#f54341", "#c81211"]
+// Gold
+colorSchemes[2] = ["#f0d588", "#e9cb82"]
+
 function setup() {
-  createCanvas(2000, 2000, SVG);
-  background(150);
+  createCanvas(w, w, SVG)
+  noStroke()
+  colorScheme = random(colorSchemes)
+  // colorScheme = colorSchemes[0]
 }
 
 function draw() {
